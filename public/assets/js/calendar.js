aac8566b14b0578c466b48455a6cc1f926e803b4
Tom Hsu <thomaschsu@gmail.com>
2018-08-22 18:47:55 -0400

    Added portal page

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,22 +1,15 @@
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-var dayNames = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-function checkTime(){
-var date = new Date();
-   var sufix = '';
-var hours = ('0'+ date.getHours()).slice(-2);
-var minutes = ('0'+ date.getMinutes()).slice(-2);
-var day = date.getDate();
-var month = monthNames[date.getMonth()];
-var weekday = dayNames[date.getDay()];
-  if(day>3 && day<21) sufix = 'th';
-  switch (day % 10) {
-        case 1:  sufix = "st";
-        case 2:  sufix =  "nd";
-        case 3:  sufix = "rd";
-        default: sufix =  "th";
-    }
-document.getElementById('time').innerHTML = "  It's <span class='hour'>" + hours + ":" + minutes + "</span><br/><span class='date'>" + month + ' ' + day + sufix +  ', ' + weekday + '.';
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
 }
-setInterval(checkTime(),500);
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
